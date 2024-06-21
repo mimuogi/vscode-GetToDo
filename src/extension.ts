66d2c2e7155a1a95ca08335b1c654a6f0963e771
mimuogi <mimuogi@gmail.com>
2024-06-21 13:41:20 +0200

    Add command - Question

--- src/extension.ts
+++ src/extension.ts
@@ -13,13 +13,19 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-gettodo.helloWorld', () => {
+	context.subscriptions.push(
+		vscode.commands.registerCommand('vscode-gettodo.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from VSCode-GetToDo!');
-	});
+			vscode.window.showInformationMessage('Hello World from Jolivert\'s Extension!');
+	}
+	));
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand("vscode-gettodo.askQuestion", () => {
+			vscode.window.showInformationMessage("How was your day?", "Good", "Bad");
+	}
+	));
 }
 
 // This method is called when your extension is deactivated
